fdbda8db9159d7f3f0ba282f1ed2eb5bf76067ed
Keerthana <keerthanagiri2000@gmail.com>
2025-09-12 19:05:25 +0530

    DB connected and index file setup

--- server/index.js
+++ server/index.js
@@ -1,1 +1,27 @@
-console.log("backend running")
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import { connectDb } from "./db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT;
+
+// Enable CORS to allow requests from frontend
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS"],
+  })
+);
+
+// Connect to MongoDB
+connectDb();
+
+// Middleware to Parse Json
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`);
+});
