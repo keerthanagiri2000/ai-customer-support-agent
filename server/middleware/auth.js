cc5675e8ddcfbb483d3b65cc37e8c24173557719
Keerthana <keerthanagiri2000@gmail.com>
2025-09-12 20:38:43 +0530

    Add auth middleware

---
+++ server/middleware/auth.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const authMiddleware = (req, res, next) => {
+    const authHeader = req.headers["authorization"];
+
+    if (!authHeader) return res.status(401).json({ message: "Authorization header missing" });
+    const token = authHeader.split(" ")[1];
+
+    if (!token) return res.status(401).json({ message: "Token missing from header" });
+
+    jwt.verify(token, process.env.JWT_SECRET_KEY, (err, decoded) => {
+        if (err) return res.status(401).json({ message: "Invalid token" });
+        req.user = decoded;
+        next();
+    });
+}
